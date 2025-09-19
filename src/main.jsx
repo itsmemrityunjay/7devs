289807b4538475c5030fedc372b0343276822c96
Abhishek <141233909+itsmemrityunjay@users.noreply.github.com>
2025-09-19 12:33:46 +0530

    vvuyvuvu

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Lenis from 'lenis'
 import App from './App.jsx'
+
+// Initialize Lenis
+const lenis = new Lenis({
+  autoRaf: true,
+});
+
+// Listen for the scroll event and log the event data
+lenis.on('scroll', (e) => {
+  console.log(e);
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
